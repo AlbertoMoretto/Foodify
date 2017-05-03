a06723a767b1c985c7822d68e3b2364ba7a5be7c
AlbertoMoretto <alberto.moretto.3@studenti.unipd.it>
2017-05-03 11:33:36 +0200

    -Add class Food

---
+++ app/src/main/java/com/esp1617/albertomoretto/foodify/Food.java
@@ -0,0 +1,21 @@
+package com.esp1617.albertomoretto.foodify;
+
+/**
+ * Created by albertomoretto on 03/05/17.
+ */
+
+public class Food {
+    private String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
