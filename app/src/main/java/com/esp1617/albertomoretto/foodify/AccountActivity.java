3c471a07e6eade567d12a44f5532683c1693c646
Leo <leo.ler94@gmail.com>
2017-07-02 17:54:07 +0200

    Aggiunti commenti

--- app/src/main/java/com/esp1617/albertomoretto/foodify/AccountActivity.java
+++ app/src/main/java/com/esp1617/albertomoretto/foodify/AccountActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+//Activity che mostra all'utente il valore del conto e gli permette di aggiungere della valuta
 public class AccountActivity extends AppCompatActivity {
 
     private TextView mBillTextView;
@@ -23,46 +24,44 @@ public class AccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account);
         mBillTextView = (TextView) findViewById(R.id.bill_text_view);
         mAddMoneyButton = (Button) findViewById(R.id.add_money_button);
+        //Prendo il valore salvato del conto e lo setto sulla TextView
         SharedPreferences sharedPref = getSharedPreferences(FoodifyTags.BILL_VALUE, Context.MODE_PRIVATE);
         savedBillValue = sharedPref.getFloat(FoodifyTags.BILL_VALUE, FoodifyConstants.DEFAULT_ACCOUNT_VALUE);
-
-
         mBillTextView.setTextColor(Color.BLACK);
         mBillTextView.setText(""+savedBillValue+"$");
+
+        //Quando si preme il tasto, il valore del conto viene incrementato di un valore standard
         mAddMoneyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //add money to your bill
                 currentBillValue = savedBillValue + FoodifyConstants.STANDARD_ACCOUNT_UPDATE_VALUE;
                 mBillTextView.setText(""+ currentBillValue +"$"); //Hardcoded $ and €
                 savedBillValue = currentBillValue;
             }
         });
     }
-
+    //Gestione del salvataggio valore del conto per apertura account tramite CheckOutActivity
     @Override
     public void onBackPressed()
     {
         super.onBackPressed();
         Intent intent = new Intent();
-
-        SharedPreferences sharedPref = getSharedPreferences(FoodifyTags.BILL_VALUE, Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putFloat(FoodifyTags.BILL_VALUE, savedBillValue);
-        editor.apply();
+        savePref();
         setResult(RESULT_OK,intent);
         finish();
     }
 
+    //Gestione del salvataggio valore del conto
     @Override
     public void onPause() {
         super.onPause();
-        SharedPreferences sharedPref = getSharedPreferences(FoodifyTags.BILL_VALUE, Context.MODE_PRIVATE);
+        savePref();
+    }
 
+    //Scrittura nelle SharedPreferences del valore attuale del conto
+    private void savePref(){
+        SharedPreferences sharedPref = getSharedPreferences(FoodifyTags.BILL_VALUE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putFloat(FoodifyTags.BILL_VALUE, savedBillValue);
         editor.apply();
-
-
     }
 }
--- app/src/main/java/com/esp1617/albertomoretto/foodify/FoodifyConstants.java
+++ app/src/main/java/com/esp1617/albertomoretto/foodify/FoodifyConstants.java
@@ -3,67 +3,31 @@ package com.esp1617.albertomoretto.foodify;
 /**
  * Created by leonardo on 04/05/17.
  */
-
+//classe che contiente tutte le costanti usate nel progetto
 public class FoodifyConstants {
 
-    public static final String HOTDOG = "hotdog";
-    public static final String FRIES = "fries";
-
-
-    public static final float STANDARD_ACCOUNT_UPDATE_VALUE = 1.5f;
-    public static final float DEFAULT_ACCOUNT_VALUE = 0.0f;
+    //Nomi di pietanze, ingredienti e dimensioni
     public static final String DEFAULT_ITEMS_READY = "";
-
+    public static final String FRIES = "fries";
     public static final String BASIC_HOTDOG_INGREDIENTS="Basic Hotdog";
-    public static final float BASIC_HOTDOG_INGREDIENTS_PRICE = 4;
-
     public static final String BASIC_FRIES_INGREDIENTS="Basic Fries";
-    public static final float BASIC_FRIES_INGREDIENTS_PRICE = 3;
-
-
-    //ingredients and foods names
     public static final String MEAT_HABURGER_CLASSIC_NAME ="Classic Hamburger";
     public static final String MEAT_HABURGER_DOUBLE_NAME ="Double Hamburger";
     public static final String MEAT_HABURGER_JUICY_NAME ="Juicy Hamburger";
-
     public static final String BREAD_HAMBURGER_SESAME_BREAD_NAME = "Sesame Bread";
     public static final String BREAD_HAMBURGER_WHOLE_BREAD_NAME = "Whole Bread";
     public static final String BREAD_HAMBURGER_BUCKWHEAT_BREAD_NAME = "Buckwheat Bread";
-
-
     public static final String BREAD_CATEGORY = "Bread";
     public static final String MEAT_CATEGORY ="Meat";
     public static final String ADDITION_CATEGORY = "Addition";
-
-    public static final float BREAD_HAMBURGER_SESAME_BREAD_PRICE = 2;
-    public static final float BREAD_HAMBURGER_WHOLE_BREAD_PRICE = 2.5f;
-    public static final float BREAD_HAMBURGER_BUCKWHEAT_BREAD_PRICE = 3;
-    public static final float MEAT_HABURGER_CLASSIC_PRICE = 3;
-    public static final float MEAT_HABURGER_DOUBLE_PRICE = 6;
-    public static final float MEAT_HABURGER_JUICY_PRICE = 4.5f;
-
-
     public static final String DRINK_SIZE_SMALL_NAME = "Small";
     public static final String DRINK_SIZE_MEDIUM_NAME = "Medium";
     public static final String DRINK_SIZE_LARGE_NAME = "Large";
-
     public static final String DRINK_TYPE_COKE_NAME = "Coke";
     public static final String DRINK_TYPE_DIET_COKE_NAME = "Diet coke";
     public static final String DRINK_TYPE_SPRITE_NAME = "Sprite";
     public static final String DRINK_TYPE_WATER_NAME = "Water";
-
     public static final String SIZE_CATEGORY = "Size";
-    public static final String TYPE_CATEGORY = "Type";
-
-
-    public static final float DRINK_SIZE_SMALL_PRICE = 1;
-    public static final float DRINK_SIZE_MEDIUM_PRICE = 2.5f;
-    public static final float DRINK_SIZE_LARGE_PRICE = 4;
-    public static final float DRINK_TYPE_COKE_PRICE = 1.5f;
-    public static final float DRINK_TYPE_DIET_COKE_PRICE = 2;
-    public static final float DRINK_TYPE_SPRITE_PRICE = 1.5f;
-    public static final float DRINK_TYPE_WATER_PRICE = 0.5f;
-
     public static final String SALAD = "Salad";
     public static final String TOMATOES = "Tomatoes";
     public static final String ONIONS = "Onions";
@@ -79,7 +43,25 @@ public class FoodifyConstants {
     public static final String LOLLIPOP = "Lollipop";
     public static final String MARSHMELLOW = "Marshmellow";
     public static final String NOUGAT = "Nougat";
+    public static final String TYPE_CATEGORY = "Type";
+    public static final String HOTDOG = "hotdog";
 
+    //Prezzi di pietanze e ingredienti
+    public static final float BREAD_HAMBURGER_SESAME_BREAD_PRICE = 2;
+    public static final float BREAD_HAMBURGER_WHOLE_BREAD_PRICE = 2.5f;
+    public static final float BREAD_HAMBURGER_BUCKWHEAT_BREAD_PRICE = 3;
+    public static final float MEAT_HABURGER_CLASSIC_PRICE = 3;
+    public static final float MEAT_HABURGER_DOUBLE_PRICE = 6;
+    public static final float MEAT_HABURGER_JUICY_PRICE = 4.5f;
+    public static final float BASIC_HOTDOG_INGREDIENTS_PRICE = 4;
+    public static final float BASIC_FRIES_INGREDIENTS_PRICE = 3;
+    public static final float DRINK_SIZE_SMALL_PRICE = 1;
+    public static final float DRINK_SIZE_MEDIUM_PRICE = 2.5f;
+    public static final float DRINK_SIZE_LARGE_PRICE = 4;
+    public static final float DRINK_TYPE_COKE_PRICE = 1.5f;
+    public static final float DRINK_TYPE_DIET_COKE_PRICE = 2;
+    public static final float DRINK_TYPE_SPRITE_PRICE = 1.5f;
+    public static final float DRINK_TYPE_WATER_PRICE = 0.5f;
     public static final float SALAD_PRICE = 0.5f;
     public static final float TOMATOES_PRICE = 0.5f;
     public static final float ONIONS_PRICE = 0.5f;
@@ -96,15 +78,20 @@ public class FoodifyConstants {
     public static final float MARSHMELLOW_PRICE = 3f;
     public static final float NOUGAT_PRICE = 3.5f;
 
-
-
-
+    //Valori di aiuto per layout
     public static final int PADDING_LEFT_RIGHT = 100;
+
     public static final int PADDING_TOP_DOWN = 180;
+    //Dimensione testo dell'ordine, valore d'aiuto per layout
     public static final int TEXT_SIZE_ORDER_MESSAGE = 35;
 
+    //Valore massimo e valore di default dell'ID di una notifica
     public static final int  MAX_NOTIFY_ID = 50000;
     public static final int DEFAULT_ORDER_ID = 1;
+
+    //Valore standard di update del conto e dell'incremento
+    public static final float STANDARD_ACCOUNT_UPDATE_VALUE = 1.5f;
+    public static final float DEFAULT_ACCOUNT_VALUE = 0.0f;
 
 
 
